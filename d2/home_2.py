cb8efbef27de7e1594cdb7275a85e736899085ae
Butcher1922 <49588203+Butcher1922@users.noreply.github.com>
2020-07-08 16:28:07 +0300

    Update home_2.py

--- d2/home_2.py
+++ d2/home_2.py
@@ -11,6 +11,10 @@ sentry_sdk.init(
 
 app = Bottle()
 
+@app.route("/")
+def home():
+    return
+
 @app.route("/success")
 def success():
     return
@@ -20,4 +24,5 @@ def fail():
     raise RuntimeError('There is an error!')
     return
 
-app.run(host='localhost',port=8080)
+port = int(os.environ.get("PORT",5000))
+app.run(host='0.0.0.0',port=port)
